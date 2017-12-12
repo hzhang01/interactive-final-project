9d66783a07ec03dda1f14a29adb0ebd3a7ac21f9
Han Zhang <h.zhang@nyu.edu>
2017-12-11 21:46:35 -0500

    Add levels

--- sketch.js
+++ sketch.js
@@ -16,11 +16,15 @@ var pic_owl, pic_dobby, pic_hat, pic_broom, pic_snitch;
 var video1;
 
 // system variables
-var mode = "development";
+var mode = "production";
 var state;
 var level = 1;
 var dots = [];
-var dots_level1_owl = [];
+var dots_level1_broom = [];
+var dots_level2_owl = [];
+var dots_level3_hat =[];
+var dots_level4_dobby= [];
+var dots_level5_snitch = [];
 var currentNumber = 1;
 
 var playing = false;
@@ -50,22 +54,58 @@ function setup() {
   playerY = height / 2;
 
   // Game setup
-  // Level 1 dots setup
-  dots_level1_owl.push(new Dot(150, 150, pic_owl, 1, 8));
-  dots_level1_owl.push(new Dot(300, 200, pic_owl, 2));
-  dots_level1_owl.push(new Dot(400, 300, pic_owl, 3));
-  dots_level1_owl.push(new Dot(500, 200, pic_owl, 4));
-  dots_level1_owl.push(new Dot(650, 150, pic_owl, 5));
-  dots_level1_owl.push(new Dot(250, 450, pic_owl, 6));
-  dots_level1_owl.push(new Dot(550, 450, pic_owl, 7));
+  // Level dots setup
+  dots_level1_broom.push(new Dot(400, 150, pic_broom, 1));
+  dots_level1_broom.push(new Dot(200, 300, pic_broom, 2));
+  dots_level1_broom.push(new Dot(600, 300, pic_broom, 3));
+  dots_level1_broom.push(new Dot(200, 450, pic_broom, 4, 5));
 
-  switch (level) {
-    case 1: dots = dots_level1_owl; break;
-  }
+  dots_level2_owl.push(new Dot(150, 150, pic_owl, 1, 8));
+  dots_level2_owl.push(new Dot(300, 200, pic_owl, 2));
+  dots_level2_owl.push(new Dot(400, 300, pic_owl, 3));
+  dots_level2_owl.push(new Dot(500, 200, pic_owl, 4));
+  dots_level2_owl.push(new Dot(650, 150, pic_owl, 5));
+  dots_level2_owl.push(new Dot(250, 450, pic_owl, 6));
+  dots_level2_owl.push(new Dot(550, 450, pic_owl, 7));
+
+  dots_level3_hat.push(new Dot(150, 150, pic_hat, 1, 8));
+  dots_level3_hat.push(new Dot(300, 300, pic_hat, 2));
+  dots_level3_hat.push(new Dot(250, 400, pic_hat, 3));
+  dots_level3_hat.push(new Dot(400, 450, pic_hat, 4));
+  dots_level3_hat.push(new Dot(550, 400, pic_hat, 5));
+  dots_level3_hat.push(new Dot(450, 300, pic_hat, 6));
+  dots_level3_hat.push(new Dot(650, 150, pic_hat, 7));
+
+  dots_level4_dobby.push(new Dot(150, 150, pic_dobby, 1,11));
+  dots_level4_dobby.push(new Dot(150, 650, pic_dobby, 2));
+  dots_level4_dobby.push(new Dot(250, 500, pic_dobby, 3));
+  dots_level4_dobby.push(new Dot(300, 550, pic_dobby, 4));
+  dots_level4_dobby.push(new Dot(450, 500, pic_dobby, 5));
+  dots_level4_dobby.push(new Dot(500, 650, pic_dobby, 6));
+  dots_level4_dobby.push(new Dot(500, 150, pic_dobby, 7));
+  dots_level4_dobby.push(new Dot(450, 300, pic_dobby, 8));
+  dots_level4_dobby.push(new Dot(300, 250, pic_dobby, 9));
+  dots_level4_dobby.push(new Dot(250, 300, pic_dobby, 10));
+
+  dots_level5_snitch.push(new Dot(300, 150, pic_snitch, 1,4));
+  dots_level5_snitch.push(new Dot(100, 450, pic_snitch, 2));
+  dots_level5_snitch.push(new Dot(500, 450, pic_snitch, 3));
+  dots_level5_snitch.push(new Dot(300, 450, pic_snitch, 5,8));
+  dots_level5_snitch.push(new Dot(200, 300, pic_snitch, 6));
+  dots_level5_snitch.push(new Dot(400, 300, pic_snitch, 7));
+
   state = "play";
 }
 
 function draw() {
+  // level dots loading
+  switch (level) {
+    case 1: dots = dots_level1_broom; break;
+    case 2: dots= dots_level2_owl;break;
+    case 3: dots = dots_level3_hat; break;
+    case 4: dots= dots_levle4_dobby; break;
+    case 5: dots= dots_level5_snitch; break;
+  }
   // Game states
   if (state === "play"){
     // Development visual aids
@@ -79,6 +119,7 @@ function draw() {
       }
       noStroke();
     }
+    background(0, 0, 0, 50);
     // Check marker
     if (markerHiro.isVisible() == true) {
       // get the position of this marker
@@ -87,12 +128,13 @@ function draw() {
       // update the player position
       playerX = hiroPosition.x;
       playerY = hiroPosition.y;
+      fill(255);
+      ellipse(width - playerX, playerY, 50, 50);
     }
     // draw the player
     // Fade style
-    background(0, 0, 0, 50);
-    fill(255);
-    ellipse(width - playerX, playerY, 50, 50);
+
+
     // draw lines
     for (var i = 0; i < dots.length; i++) {
       if(i !== 0 && currentNumber - 1 > i) {
@@ -101,17 +143,18 @@ function draw() {
         line(dots[i - 1].x, dots[i - 1].y, dots[i].x, dots[i].y);
         noStroke();
         noFill();
+
       }
-    }
-    // draw last line / puzzle complete
-    if (dots[0].lastChecked) {
-      var last = dots.length - 1;
-      stroke(40);
-      fill('green');
-      line(dots[last].x, dots[last].y, dots[0].x, dots[0].y);
-      noStroke();
-      noFill();
-      state = "video";
+      // draw last line / puzzle complete
+      if (dots[i].lastChecked) {
+        var last = dots.length - 1;
+        stroke(40);
+        fill('green');
+        line(dots[last].x, dots[last].y, dots[0].x, dots[0].y);
+        noStroke();
+        noFill();
+        state = "video";
+      }
     }
     // draw dots
     for (var i = 0; i < dots.length; i++) {
@@ -121,11 +164,12 @@ function draw() {
   }
   else if (state === "video"){
     if (!playing) {
-      video1 = createVideo("pic/video1.mp4");
+      video1 = createVideo("pic/video" + level + ".mp4");
       video1.autoplay();
       video1.onended( function () {
         video1.hide();
         state = 'play';
+        level += 1;
       });
       playing = true;
     }
@@ -170,6 +214,7 @@ function Dot (xPos, yPos, artwork, number, altNumber) {
       ellipse(this.x, this.y, 50, 50, 100, 100);
       noFill();
     }
+    fill("white");
     image(this.artwork, this.x, this.y, 50, 50);
     textSize(30);
     if(this.altNumber !== 0){
@@ -177,6 +222,5 @@ function Dot (xPos, yPos, artwork, number, altNumber) {
     } else {
       text(this.number,this.x + 25, this.y);
     }
-
   }
 }
